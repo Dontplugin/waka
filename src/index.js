726ed3dd5a9222fcc348444b152dda0c9392f37a
韩宇哲 <hanyuzhe@souche.com>
2019-07-05 15:44:02 +0800

    git init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import AudioAnalyser from 'react-audio-analyser'
+
+
+export default class App extends React.Component {
+
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            status: "",
+            audioType: 'audio/wav'
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    controlAudio(status) {
+        this.setState({
+            status
+        })
+    }
+
+    changeScheme(e) {
+        this.setState({
+            audioType: e.target.value
+        })
+    }
+
+    render() {
+        const {status, audioSrc, audioType} = this.state;
+        const audioProps = {
+            audioType,
+            audioOptions: {sampleRate: 8000}, // 设置输出音频采样率
+            status,
+            audioSrc,
+            timeslice: 1000, // timeslice（https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder/start#Parameters）
+            startCallback: (e) => {
+                console.log("succ start", e)
+            },
+            pauseCallback: (e) => {
+                console.log("succ pause", e)
+            },
+            stopCallback: (e) => {
+                this.setState({
+                    audioSrc: window.URL.createObjectURL(e)
+                })
+                console.log("succ stop", e)
+            },
+            onRecordCallback: (e) => {
+                console.log("recording", e)
+            },
+            errorCallback: (err) => {
+                console.log("error", err)
+            }
+        }
+        return (
+            <div>
+                <AudioAnalyser {...audioProps}>
+                    <div className="btn-box">
+                        {status !== "recording" &&
+                        <i className="iconfont icon-start" title="开始"
+                           onClick={() => this.controlAudio("recording")}></i>}
+
+                        {status === "recording" &&
+                        <i className="iconfont icon-pause" title="暂停"
+                           onClick={() => this.controlAudio("paused")}></i>}
+                           
+                        <i className="iconfont icon-stop" title="停止"
+                           onClick={() => this.controlAudio("inactive")}></i>
+                    </div>
+                </AudioAnalyser>
+                <p>输出文件类型</p>
+                <select name="" id="" onChange={(e) => this.changeScheme(e)} value={audioType}>
+                    <option value="audio/webm">audio/webm（default）</option>
+                    <option value="audio/wav">audio/wav</option>
+                    <option value="audio/mp3">audio/mp3</option>
+                </select>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
